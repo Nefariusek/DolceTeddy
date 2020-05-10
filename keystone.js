9ee74ee7df757a9a53fd00425257eb226268d6b0
Szymon <szymonpakula98@gmail.com>
2020-05-10 02:47:56 +0200

    First commit, keystone init, hello world site

---
+++ keystone.js
@@ -0,0 +1,50 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+
+// Require keystone
+const keystone = require('keystone');
+
+// Initialise Keystone with your project's configuration.
+// See http://keystonejs.com/guide/config for available options
+// and documentation.
+
+keystone.init({
+	'name': 'DolceTeddy',
+	'brand': 'DolceTeddy',
+	'auto update': true,
+	'session': true,
+	'auth': true,
+	'user model': 'User',
+});
+
+// Load your project's Models
+keystone.import('models');
+
+// Setup common locals for your templates. The following are required for the
+// bundled templates and layouts. Any runtime locals (that should be set uniquely
+// for each request) should be added to ./routes/middleware.js
+app.prepare().then(() => {
+	keystone.set('locals', {
+		_: require('lodash'),
+		env: keystone.get('env'),
+		utils: keystone.utils,
+		editable: keystone.content.editable,
+	});
+
+	// Load your project's Routes
+	keystone.set('routes', require('./routes')(app));
+
+	// Configure the navigation bar in Keystone's Admin UI
+	keystone.set('nav', {
+		users: 'users',
+	});
+
+	// Start Keystone to connect to your database and initialise the web server
+
+	keystone.start();
+});
